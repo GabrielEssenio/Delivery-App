82c1847b6cb3053a9882a8df00cffb1eb4bdfecf
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-11-26 10:23:55 -0300

    Stylizando pagina de todos produtos

--- front-end/src/styles/Produtos.js
+++ front-end/src/styles/Produtos.js
@@ -4,7 +4,7 @@ export const contentProduct = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
-  justify-content: space-around;
+  justify-content: space-evenly;
   flex-wrap: wrap;
 `;
 
@@ -13,17 +13,23 @@ export const eachProduct = styled.div`
   flex-direction: column;
   border-radius: 5px;
   box-shadow: 7px 7px 13px 0px rgba(50, 50, 50, 0.22);
-  width: 20vw;
+  width: 30%;
+  background-color: #ffffff;
+  margin-top: 10px;
+  margin-bottom: 20px;
   transition: all 0.3s ease-out;
   &:hover {
-    transform: translateY(-8px);
-    cursor: pointer;
-  }
+  -webkit-transform: scale(1.2);
+  -ms-transform: scale(1.2);
+  transform: scale(1.2);
+}
 `;
 
 export const priceProduct = styled.h2`
-  background-color: #f2fffc;
+  background-color: #333333;
+  color: #FFC500;
   width: 20%;
+  text-align: center;
   border-radius:5px;
 `;
 
@@ -35,10 +41,9 @@ export const imgProduct = styled.img`
 `;
 
 export const nameProduct = styled.p`
-  font-family: Roboto;
-  font-style: normal;
   align-self: center;
-  font-size: 20px;
+  font-size: 24px;
+  color: #333333;
 `;
 export const qtyProduct = styled.div`
   display: flex;
@@ -47,11 +52,13 @@ export const qtyProduct = styled.div`
 `;
 
 export const buttonProduct = styled.button`
-  background: #036b52;
-  color: #fff;
+   background-color: #333333;
+  color: #FFC500;
   border: 1px;
+  width: 7%;
   border-radius: 20%;
 `;
 export const inputProduct = styled.input`
-  width: 10%;
+  width: 20%;
+  text-align: center;
 `;
